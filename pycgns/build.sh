3ee4bba813c37492796b337225d2c0cd9bbcb8e0
Mickael Philit <mickey.phy@gmail.com>
2017-08-22 08:41:15 +0200

    update pycgns anaconda recipe for windows

--- pycgns/build.sh
+++ pycgns/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+export INCLUDE_PATH="${PREFIX}/include:${INCLUDE_PATH}"
 
-python setup.py build
+python setup.py build  --generate
 
 python setup.py install --prefix=${PREFIX}
