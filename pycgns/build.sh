3a4eed4b4b099a11df9d7bd5ad538f0ecf7f10fb
MicK7 <mickey.phy@gmail.com>
2022-05-11 17:17:29 +0200

    Delete build.sh

--- pycgns/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-export INCLUDE_PATH="${PREFIX}/include:${INCLUDE_PATH}"
-
-python setup.py build
-
-python setup.py install --prefix=${PREFIX}
