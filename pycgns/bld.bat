e325d4d15fb2a3b0d602dc196592657ee99b28b3
MicK7 <mickey.phy@gmail.com>
2022-05-11 17:17:20 +0200

    Delete bld.bat

--- pycgns/bld.bat
+++
@@ -1,8 +0,0 @@
-
-
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib" --generate
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py install --prefix="%PREFIX%"
-if errorlevel 1 exit 1
-
